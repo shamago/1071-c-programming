e64dd865a05f430e2e7f2f8747c1a4a6fc087ecf
shamago <43315152+shamago@users.noreply.github.com>
2018-11-06 15:17:04 +0800

    Create fib.cpp

---
+++ w07/fib.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+long fib(int n){
+    if(n==0 || n==1)return n;
+    else
+        return fib(n-1)+fib(n-2);
+}
+
+int main(){
+    printf("fib(3) = %ld\n",fib(3));
+    printf("fib(10) = %ld\n",fib(10));
+    printf("fib(30) = %ld\n",fib(30));
+}
